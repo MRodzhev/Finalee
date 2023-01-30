5a3445fdc614f08ac4d47ee5d721d383f8c31c1b
Martin Georgiev Rodzhev <106818714+MRodzhev@users.noreply.github.com>
2023-01-30 22:47:06 +0200

    Update outputs.tf

--- modules/backend/outputs.tf
+++ modules/backend/outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_arn" {
-  value = aws_s3_bucket.s3_bucket.arn
+  value = aws_s3_bucket.s3-bucket.arn
 }
 
 output "bucket_name" {
-  value = aws_s3_bucket.s3_bucket.bucket_domain_name
+  value = aws_s3_bucket.s3-bucket.bucket_domain_name
 }
 output "s3_bucket" {
     value = aws_s3_bucket.s3-bucket.id
