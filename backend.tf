c63e08cd48eeac0af57573310fa879026e8b5aad
Martin Georgiev Rodzhev <106818714+MRodzhev@users.noreply.github.com>
2023-01-30 22:54:17 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 
-    #backend "s3" {
-       # encrypt = true
-       # bucket  = "mrodzhev-s3-bucket"
-        #region  = "eu-central-1"
-       # key     = "terraform.tfstate"
-    #}
-#}
+    backend "s3" {
+        encrypt = true
+        bucket  = "mrodzhev-s3-bucket"
+        region  = "eu-central-1"
+        key     = "terraform.tfstate"
+    }
+}
