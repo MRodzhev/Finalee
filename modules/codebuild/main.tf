4d4f127d430bd0bb4379d59c33a29ed06a74cb22
Martin Georgiev Rodzhev <106818714+MRodzhev@users.noreply.github.com>
2023-01-31 15:55:07 +0200

    Update main.tf

--- modules/codebuild/main.tf
+++ modules/codebuild/main.tf
@@ -3,14 +3,16 @@ data "aws_region" "current" {}
 resource "aws_security_group" "CodebuildSG" {
   name        = "CodebuildSG"
   vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+ 
+ dynamic "ingress" {
+    for_each = var.allowed_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      security_groups = [aws_security_group.AppLBSG.id]
+    }
   }
-
   egress {
     from_port   = 0
     to_port     = 0
