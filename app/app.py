a894a95f1a6cfe922af51af5f0bafa9e6fe29c06
Martin Georgiev Rodzhev <106818714+MRodzhev@users.noreply.github.com>
2023-02-03 15:25:45 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,8 +3,12 @@ import random
 app = Flask(__name__)
 # list of cat images
 images = [
-  "https://tenor.com/view/jack-russell-jack-russell-terrier-jack-russell-gif-13114603"
-  "https://tenor.com/view/cute-adorable-doggo-puppy-grin-gif-24799385"
+  "https://media1.giphy.com/media/BzyTuYCmvSORqs1ABM/giphy.gif?cid=ecf05e47a61pohrnxvkjt8ax30omfxqbla893upt6n822900&rid=giphy.gif&ct=g",
+  "https://media1.giphy.com/media/ICOgUNjpvO0PC/giphy.gif?cid=ecf05e47a61pohrnxvkjt8ax30omfxqbla893upt6n822900&rid=giphy.gif&ct=g",
+  "https://media0.giphy.com/media/MDJ9IbxxvDUQM/giphy.gif?cid=ecf05e47a61pohrnxvkjt8ax30omfxqbla893upt6n822900&rid=giphy.gif&ct=g",
+  "https://media0.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif?cid=ecf05e47a61pohrnxvkjt8ax30omfxqbla893upt6n822900&rid=giphy.gif&ct=g",
+  "https://media.tenor.com/zrpyKEyxZGwAAAAd/fat-cat-laser-eyes.gif"
+ 
 ]
 @app.route('/')
 def index():
